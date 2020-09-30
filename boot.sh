7fb21f3ad756c3b1f4a6d0da061611b9bf5801a9
takuto <takutoitoi914@ruri.waseda.jp>
2020-10-01 05:27:26 +0900

    fixed bugs

--- boot.sh
+++ boot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export RUN_PLACE='localhost'
 export DATABASE_URL='sqlite:///app/database.sqlite3'
+export HASHID_SALT='tascrewLOCAL'
 
 python create_events.py $@
--- frontend/config/local.env.js
+++ frontend/config/local.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   VUE_APP_BASE_URL: '"http://localhost:8080"',
   BUILD_TYPE: '"local"',
-  HASHID_SALT: '"CalendarTmp"'
+  HASHID_SALT: '"tascrewLOCAL"'
 }
