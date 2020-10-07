bad63920ce26b40ed8f7a4fe0a63446911170cc3
takuto <takutoitoi914@ruri.waseda.jp>
2020-10-08 00:25:29 +0900

    fixed bug

--- frontend/src/components/SettingsSidebar.vue
+++ frontend/src/components/SettingsSidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <ul id="settings_sidebar" class="nav flex-column nav-pills pl-3 pr-3 pr-md-0 py-2">
     <!-- <li class="col-12" @click="$router.push('/settings/calendarapi')">Calendar API Setting</li> -->
-    <li v-for="(n, index) in setting_list.map(x => x.toLowerCase())" :key="n" class="nav-item" @click="$router.push(`/usersettings/${n}`)">
+    <li v-for="(n, index) in setting_list.map(x => x.toLowerCase())" :key="n" class="nav-item" @click="$router.push(`/usersettings?menu=${n}`)">
       <a class="btn-light" :class="`rounded-0 nav-link ${is_active(n, index)}`" style="border-top-left-radius: .25rem!important; border-bottom-left-radius: .25rem!important;">
         {{ $t(`Settings.${n}`) }}
       </a>
