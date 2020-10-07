05ee1bdf518123661ee1e9acde4276fe95818168
takuto <takutoitoi914@ruri.waseda.jp>
2020-10-07 23:07:02 +0900

    router beforeEach moved to router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
+import mixin from '@/mixin'
 import dictionary from '@/lang/dictionary.json'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -14,51 +15,17 @@ const gauthOption = {
   prompt: 'select_account'
 }
 
+Vue.mixin(mixin)
 Vue.use(GAuth, gauthOption)
 Vue.use(BootstrapVue)
 Vue.use(VueI18n)
 Vue.config.productionTip = true
+Vue.config.devtools = true
 
 const i18n = new VueI18n({
   locale: ((window.navigator.languages && window.navigator.languages[0]) || window.navigator.language || window.navigator.userLanguage || window.navigator.browserLanguage).includes('ja') ? 'ja' : 'en',
   fallbackLocale: 'ja',
   messages: dictionary
-})
-
-const checkHashID = async (id) => {
-  if (store.dispatch('checkValidHashID', id)) return true
-  else {
-    await store.dispatch('refreshValidHashID', [store.dispatch('decodeHashID', id).type])
-    return store.dispatch('checkValidHashID', id)
-  }
-}
-
-router.beforeEach(async (to, from, next) => { // eslint-disable-line no-unused-vars
-  if (to.matched.some(record => record.meta.requiredAuth) && !store.getters.is_loggedin) {
-    if (!(await store.dispatch('checkIsLoggedin'))) { // check if login is needed
-      to.query['nexturl'] = to.path
-      next({
-        name: 'tryaccess',
-        params: { page: 'login' },
-        query: to.query
-      })
-      return
-    }
-  }
-  if (to.matched.some(record => record.meta.checkHashID)) {
-    if (to.params.id && !(await checkHashID(to.params.id))) { // check if this project/company/user is valid
-      alert('invalid project id')
-      next({
-        name: 'notfound'
-      })
-      return
-    }
-    await store.dispatch( // アクセス権限を確認してない要素は更新する
-      'refreshValidHashID',
-      store.state.accessType.filter(e => store.getters.getValidAccess[e] === undefined)
-    )
-  }
-  next()
 })
 
 new Vue({
