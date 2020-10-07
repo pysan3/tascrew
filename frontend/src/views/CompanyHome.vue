ec45ebfccb7e8d8ffbee0fb82799bb5fe139f3f6
takuto <takutoitoi914@ruri.waseda.jp>
2020-10-07 23:09:06 +0900

    project, companies home

--- frontend/src/views/CompanyHome.vue
+++ frontend/src/views/CompanyHome.vue
@@ -1,4 +1,21 @@
 <template>
   <div id="companyhome">
+    <h1>企業ホーム</h1>
+    <div v-for="(v, k) in elementdata" :key="k">
+      {{ k }} : {{ v }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      elementdata: {}
+    }
+  },
+  async created () {
+    this.elementdata = await this.$_accessInformation(this.$route.params.id)
+  }
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home" class="row no-gutters">
     <h1>HOME</h1>
-    <h2><router-link to="/usersettings/calendarapi">Calendar API Settings</router-link></h2>
+    <h2><router-link to="/usersettings?mode=calendarapi">Calendar API Settings</router-link></h2>
   </div>
 </template>
